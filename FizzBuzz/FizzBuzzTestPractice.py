bb849d56c142ae0382a50c8c119d71aa11cec937
Melissa <melissascampini@gmail.com>
2019-07-14 10:43:47 -0700

    exercise completed

--- FizzBuzz/FizzBuzzTestPractice.py
+++ FizzBuzz/FizzBuzzTestPractice.py
@@ -2,23 +2,46 @@ import pytest
 
 
 def fizzbuzz(value):
-    if value == 3:
+    if is_multiple(value, 3):
+        if is_multiple(value, 5):
+            return 'FizzBuzz'
         return 'Fizz'
+
+    if is_multiple(value, 5):
+        return 'Buzz'
     return str(value)
 
+def is_multiple(value, mod):
+    return (value % mod) == 0
 
-def checkfizzbuzz(value, expectedretval):
+def check_fizzbuzz(value, expectedretval):
     retval = fizzbuzz(value)
     assert retval == expectedretval
 
 
 def test_return1with1passedin():
-    checkfizzbuzz(1, '1')
+    check_fizzbuzz(1, '1')
 
 
 def test_return2with2passedin():
-    checkfizzbuzz(2, '2')
+    check_fizzbuzz(2, '2')
 
 
 def test_returnfizzwhen3passedin():
-    checkfizzbuzz(3, 'fizz')
+    check_fizzbuzz(3, 'Fizz')
+
+
+def test_returnbuzzwhen5passedin():
+    check_fizzbuzz(5, 'Buzz')
+
+
+def test_returnfizzwhen6passedin():
+    check_fizzbuzz(6, 'Fizz')
+
+
+def test_returnbuzzwhen10passedin():
+    check_fizzbuzz(10, 'Buzz')
+
+
+def test_returnfizzbuzzwith15passedin():
+    check_fizzbuzz(15, 'FizzBuzz')
